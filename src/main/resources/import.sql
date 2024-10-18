e28f9dc7ac799f0a6e38625b47305597c8b8cbcf
virendra.shinde <virendra.shinde@honeywell.com>
2024-10-18 18:25:14 +0530

    Added import.sql for H2 DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (id, name, description, price, quantity) VALUES (1, 'Apple I phone', 'IOS device', 50000, 10);
+INSERT INTO products (id, name, description, price, quantity) VALUES (2, 'Samsung S23', 'Android device', 90000, 75);
+INSERT INTO products (id, name, description, price, quantity) VALUES (3, 'One Plus 6T', 'Android device', 45000, 50);
+INSERT INTO products (id, name, description, price, quantity) VALUES (4, 'Noting One', 'NOS device', 30000, 100);
+INSERT INTO products (id, name, description, price, quantity) VALUES (5, 'Google Pixel', 'Google device', 55000, 5);
